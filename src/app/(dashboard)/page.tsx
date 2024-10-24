ef72f093d146b5101760b9e5726bec6a9a416c3c
Matheus Silva <msilvaqs@outlook.com>
2024-10-24 11:22:40 -0400

    Creating workspace api and form

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,15 @@
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/src/features/auth/actions";
+import { CreateWorkspaceForm } from "@/src/features/workspaces/components/create-workspace-form";
 
 export default async function Home() {
   const user = await getCurrentUser();
 
   if (!user) redirect("/sign-in");
 
-  return <div>This is a home page</div>;
+  return (
+    <div className="bg-neutral-500 p-4 h-full">
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
